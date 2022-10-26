533be87edb04f4037fc268c11dfe6a103e43667b
BahromjonPolat <itjunior.uz@gmail.com>
2022-10-26 22:36:01 +0500

    14. Longest Common Prefix was started

---
+++ easy/14_longest_common_prefix/longest_common_prefix.dart
@@ -0,0 +1,33 @@
+/*
+    Author: Bakhromjon Polat
+
+    Github: https://github.com/BahromjonPolat
+    LinkedIn: https://linkedin.com/in/bahromjon-polat
+    Telegram: https://t.me/BahromjonPolat
+
+    Started on: Oct 26 2022 22:25:31
+    Solved on: 
+
+    Runtime: 
+    Memory: 
+
+    Title: 14. Longest Common Prefix
+    Problem:
+
+      Write a function to find the longest common prefix string amongst an array of strings.
+      If there is no common prefix, return an empty string "".
+
+    Link: https://leetcode.com/problems/longest-common-prefix/
+
+*/
+
+void main() {}
+
+///Solution
+String longestCommonPrefix(List<String> strs) {
+  int len = 0;
+  for (var i = 0; i < strs.length; i++) {
+ 
+  }
+  return '';
+}
