b9d1b5c68551ccce63733b08cfe812381bb09159
BahromjonPolat <itjunior.uz@gmail.com>
2022-10-29 18:33:41 +0500

    folders renamed

--- easy/0231_power_of_two/power_of_two.dart
+++ easy/0231_power_of_two/power_of_two.dart
@@ -14,27 +14,45 @@
     Title: 231. Power of Two
     Problem:
 
-      Given an integer n, return true if it is a power of two. Otherwise, return false.
-
-An integer n is a power of two, if there exists an integer x such that n == 2x.
+      Given an integer n, return true if it is a power of two. Otherwise, 
+      return false. An integer n is a power of two, if there exists an integer 
+      x such that n == 2x.
 
     Link: https://leetcode.com/problems/power-of-two/
 
 */
 
 void main() {
-  print(isPowerOfTwo(5));
+  print(isPowerOfTwo2(2));
 }
 
 ///Solution
 bool isPowerOfTwo(int n) {
-  if (n == 1) return true;
+  if (n == 1 || n == 2) {
+    return true;
+  }
   while (true) {
     double a = n / 2;
     if (a % 2 != 0) {
       break;
     }
     if (a == 2) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+bool isPowerOfTwo2(int n) {
+  if (n == 1 || n == 2) return true;
+  while (true) {
+    if (n % 2 != 0) {
+      break;
+    } else {
+      n = n ~/ 2;
+    }
+    if (n == 2) {
       return true;
     }
   }
