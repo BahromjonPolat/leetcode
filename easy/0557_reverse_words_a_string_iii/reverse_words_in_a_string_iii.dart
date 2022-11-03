fcaf27909312c18e52b31f2b3c02149073c04a06
BahromjonPolat <itjunior.uz@gmail.com>
2022-11-03 23:14:45 +0500

    557. Reverse Words in a String III in dart

---
+++ easy/0557_reverse_words_a_string_iii/reverse_words_in_a_string_iii.dart
@@ -0,0 +1,36 @@
+/*
+    Author: Bakhromjon Polat
+
+    Github: https://github.com/BahromjonPolat
+    LinkedIn: https://linkedin.com/in/bahromjon-polat
+    Telegram: https://t.me/BahromjonPolat
+
+    Started on: Nov 03 2022 23:00:51
+    Solved on: Nov 03 2022 23:08:51
+
+    Runtime: 734 ms
+    Memory: 149.6 MB
+
+    Title: 557. Reverse Words in a String III
+    
+    Problem:
+
+      Given a string s, reverse the order of characters in each word within a 
+      sentence while still preserving whitespace and initial word order.
+
+    Link: https://leetcode.com/problems/reverse-words-in-a-string-iii/
+
+*/
+
+void main() {
+  print(reverseWords("Let's take LeetCode contest"));
+}
+
+///Solution
+String reverseWords(String s) {
+  List<String> words = [];
+  for (var word in s.split(' ')) {
+    words.add(word.split('').reversed.join());
+  }
+  return words.join(' ');
+}
